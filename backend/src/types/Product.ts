4bb0d8b68a4728a48549edd4a2b52a1cc47d9117
ali <moini_alessandro_13@yahoo.com>
2023-05-19 14:14:50 +0430

    create node server

---
+++ backend/src/types/Product.ts
@@ -0,0 +1,17 @@
+export type Product = {
+  //   _id: string;
+  name: string;
+  image: string;
+  //   banner: string;
+  category: string;
+  brand: string;
+  description: string;
+  // reviews: Review[]
+  slug: string;
+  //   images: string[];
+  rating: number;
+  numReviews: number;
+  price: number;
+  countInStock: number;
+  //   isFeatured: boolean;
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,21 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 function App() {
   return (
     <div className="d-flex flex-column vh-100">
-      <header>TS Amazona</header>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand>tsamazona</Navbar.Brand>
-        </Container>
-        <Nav>
-          <a href="/cart" className="nav-link">
-            Cart
-          </a>
-          <a href="/signin" className="nav-link">
-            Sign In
-          </a>
-        </Nav>
-      </Navbar>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>tsamazona</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
         <Container className="mt-3">
           <Outlet />
