8c97118a4abb5f82396f257259e177bfc0b599b4
ali <moini_alessandro_13@yahoo.com>
2023-05-24 19:18:55 +0430

    productPage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,18 @@ import express, { Request, Response } from 'express';
 import { sampleProducts } from './data';
 const app = express();
 app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:5173'],
-  })
+  cors()
+  //   {
+  //   credentials: true,
+  //   origin: ['http://localhost:5173'],
+  // }
 );
 app.get('/api/products', (req: Request, res: Response) => {
   res.json(sampleProducts);
+});
+
+app.get('/api/products/:slug', (req: Request, res: Response) => {
+  res.json(sampleProducts.find((x) => x.slug === req.params.slug));
 });
 const PORT = 4000;
 app.listen(PORT, () => {
