af9cc1977f80bfd644979b8bca53d774d6cf918f
ali <moini_alessandro_13@yahoo.com>
2023-08-04 09:54:57 +0430

    changing cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,12 +23,12 @@ mongoose
   });
 const app = express();
 app.use(
-  cors()
-  //   {
-  //   credentials: true,
-  //   origin: ['http://localhost:5173'],
-  // }
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173', '/'],
+  })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
